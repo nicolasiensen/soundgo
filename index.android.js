1059c9f8abc21c8a65855299cf7ed9c7a5954d43
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-03-26 17:59:02 +0100

    Add eslint config file

--- index.android.js
+++ index.android.js
@@ -1,10 +1,10 @@
-'use strict';
+'use strict'
 
-import React from 'react-native';
-import Button from 'react-native-button';
-import superagent from 'superagent';
-import RNFS from 'react-native-fs';
-const { StyleSheet, AppRegistry, View, Text, ListView, TouchableHighlight } = React;
+import React from 'react-native'
+import Button from 'react-native-button'
+import superagent from 'superagent'
+import RNFS from 'react-native-fs'
+const { StyleSheet, AppRegistry, View, Text, ListView, TouchableHighlight } = React
 
 const soundgo = React.createClass ({
   loadTracks () {
@@ -12,12 +12,13 @@ const soundgo = React.createClass ({
       .get('https://api.soundcloud.com/tracks')
       .query({client_id: '613b49e595474fe66d19172652fe8423'})
       .end((err, res) => {
+        console.log(res)
         this.setState({
           tracks: this.state.tracks.cloneWithRows(
             res.body.filter((track) => track.downloadable)
           )
         })
-      });
+      })
   },
 
   downloadTrack (track) {
@@ -30,7 +31,9 @@ const soundgo = React.createClass ({
           `/mnt/sdcard/${track.id}.${track.original_format}`,
           (start) => {console.log(start)},
           (progress) => {console.log(progress)}
-        )
+        ).then((success) => {
+          console.log('Download finished!')
+        })
       }
     )
   },
@@ -42,7 +45,7 @@ const soundgo = React.createClass ({
           rowHasChanged: (row1, row2) => row1 !== row2,
         })
       }
-    );
+    )
   },
 
   renderTrack (track) {
@@ -56,7 +59,7 @@ const soundgo = React.createClass ({
   },
 
   render() {
-    const tracks = this.state.tracks;
+    const tracks = this.state.tracks
     return (
       <View style={styles.container}>
         <Button
@@ -71,7 +74,7 @@ const soundgo = React.createClass ({
           style={styles.listView}
         />
       </View>
-    );
+    )
   }
 })
 
@@ -97,6 +100,6 @@ const styles = StyleSheet.create({
   track: {
     padding: 20
   }
-});
+})
 
-AppRegistry.registerComponent('soundgo', () => soundgo);
+AppRegistry.registerComponent('soundgo', () => soundgo)
